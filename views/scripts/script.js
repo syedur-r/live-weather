d69dcf130b3c50fb4d027988b1950665ea415d6b
Syedur Rahman <36609668+syedur-r@users.noreply.github.com>
2021-06-07 04:15:37 +0100

    Update script.js

--- views/scripts/script.js
+++ views/scripts/script.js
@@ -1,3 +1,3 @@
-document.body.style.background = "linear-gradient(to bottom, rgba(0, 0, 0, .7), rgba(0, 0, 0, .5)), url('https://source.unsplash.com/1600x900/?" +  document.getElementById("city").innerHTML + ", city')";
+document.body.style.background = "linear-gradient(to bottom, rgba(0, 0, 0, .7), rgba(0, 0, 0, .5)), url('https://source.unsplash.com/"+ window.innerWidth + "x" + window.innerHeight + "/?" +  document.getElementById("city").innerHTML + ", city')";
 document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundSize = "cover";
